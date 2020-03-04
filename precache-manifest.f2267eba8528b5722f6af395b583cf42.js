32e78e70e7342e0af91100c549897de0f3dd1307
mindelias@gmail.com <mindelias@gmail.com>
2020-03-04 09:53:16 +0100

    make changes

---
+++ precache-manifest.f2267eba8528b5722f6af395b583cf42.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693c2cab91d5f08f2873ee5d71c14416",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5c144f1a2a69595c2cf",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2635ba964fda1d6aa73a",
+    "url": "/static/css/main.9fa9c09e.chunk.css"
+  },
+  {
+    "revision": "a5c144f1a2a69595c2cf",
+    "url": "/static/js/2.57290df0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.57290df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2635ba964fda1d6aa73a",
+    "url": "/static/js/main.bf4039f2.chunk.js"
+  },
+  {
+    "revision": "a8c9c4166115031740b5",
+    "url": "/static/js/runtime-main.6ca7e2bc.js"
+  },
+  {
+    "revision": "a6991c97f101d6cf1908d843c0c54611",
+    "url": "/static/media/avatar.a6991c97.png"
+  },
+  {
+    "revision": "afc38922d257065b0f4141c6bfbfe9b1",
+    "url": "/static/media/circle.afc38922.jpg"
+  },
+  {
+    "revision": "b5a18954045f8a62c1d4203e6c7f95d0",
+    "url": "/static/media/finance.b5a18954.svg"
+  },
+  {
+    "revision": "8dc4e77f998d86968ec3f5200a3bbc75",
+    "url": "/static/media/float.8dc4e77f.png"
+  },
+  {
+    "revision": "8b4fe9e84ef98cadd0f565e98ce5f496",
+    "url": "/static/media/ladycoin.8b4fe9e8.jpeg"
+  }
+]);
